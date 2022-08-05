c3bf3029b61804df0c33d72013cd1be76c0f7f5f
Timothy Harris <timothy.harris@kikoda.com>
2022-08-05 15:14:19 +0000

    init

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,12 +1,78 @@
-import { typescript } from 'projen';
-const project = new typescript.TypeScriptProject({
-  defaultReleaseBranch: 'main',
-  name: 'generated-config-hooks',
-  projenrcTs: true,
+import { KikodaOpenSourceProject } from '@kikoda/projen-templates';
+import { YamlFile } from 'projen';
+import { GithubCredentials } from 'projen/lib/github';
+import { ArrowParens, EndOfLine, TrailingComma, TypeScriptJsxMode } from 'projen/lib/javascript';
+import { TypeScriptProject } from 'projen/lib/typescript';
 
-  // deps: [],                /* Runtime dependencies of this module. */
-  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
-  // devDeps: [],             /* Build dependencies for this module. */
-  // packageName: undefined,  /* The "name" in package.json. */
+const project = new TypeScriptProject({
+  name: 'kikoda-generated-config-hooks',
+  description: 'React hooks companion library for @kikoda/generated-config.',
+  authorName: 'Kikoda, LLC',
+  authorEmail: 'platform@kikoda.com',
+  repository: 'https://github.com/KikodaCode/kikoda-generated-config-hooks.git',
+  defaultReleaseBranch: 'main',
+  keywords: ['configuration', 'websites', 'cicd', 'webpack', 'react'],
+  stability: 'experimental',
+  license: 'Apache-2.0',
+  projenrcTs: true,
+  devContainer: true,
+  vscode: true,
+  prettier: true,
+  releaseToNpm: true,
+  prettierOptions: {
+    settings: {
+      printWidth: 100,
+      tabWidth: 2,
+      useTabs: false,
+      semi: true,
+      singleQuote: true,
+      trailingComma: TrailingComma.ALL,
+      arrowParens: ArrowParens.AVOID,
+      endOfLine: EndOfLine.LF,
+    },
+  },
+  bundledDeps: [],
+  tsconfig: {
+    compilerOptions: { esModuleInterop: true, lib: ['dom'], jsx: TypeScriptJsxMode.REACT },
+  },
+  devDeps: ['@kikoda/projen-templates', 'react', '@types/react'],
+  peerDeps: ['webpack', 'react'],
+  packageName: '@kikoda/generated-config-hooks',
+  gitignore: [],
+  githubOptions: {
+    projenCredentials: GithubCredentials.fromApp(),
+  },
+  pullRequestTemplate: false,
+  codeCov: true,
+  autoApproveUpgrades: true,
+  autoApproveOptions: {
+    allowedUsernames: ['projen-workflows[bot]'],
+  },
+  jestOptions: {
+    jestConfig: {
+      coveragePathIgnorePatterns: ['/node_modules/', 'test/config'],
+    },
+  },
 });
+
+// Run Build workflow on push to main to update base code coverage
+const buildWorkflow = project.github?.tryFindWorkflow('build');
+buildWorkflow?.on({
+  push: { branches: ['main'] },
+});
+
+new YamlFile(project, 'codecov.yml', {
+  obj: {
+    coverage: {
+      status: {
+        patch: true,
+      },
+    },
+  },
+});
+
+new KikodaOpenSourceProject(project, {
+  title: 'React hooks for @kikoda/generated-config',
+});
+
 project.synth();
--- README.md
+++ README.md
@@ -1,1 +1,41 @@
-# replace this
+[<img src="https://kikoda.com/wp-content/uploads/2019/07/Logo_White_bg.svg" width="300"/>](https://kikoda.com)
+# React hooks for @kikoda/generated-config
+
+[![NPM](https://img.shields.io/npm/v/@kikoda/generated-config-hooks?color=39a356)](https://www.npmjs.com/package/@kikoda/generated-config-hooks)
+[![License](https://img.shields.io/badge/license-Apache--2.0-blue)](https://github.com/KikodaCode/kikoda-generated-config-hooks.git/blob/main/LICENSE)
+
+Use this React hooks for @kikoda/generated-config to architect and model modern applications
+
+## Install from NPM:
+```
+yarn add --dev @kikoda/generated-config-hooks
+
+# or
+
+npm install @kikoda/generated-config-hooks --save-dev
+```
+
+## Usage
+<!-- TODO: add Usage & Examples -->
+
+## Opening Issues
+
+If you encounter a bug with this package, we want to hear about it. Before opening a new issue, search the existing issues to avoid duplicates.
+
+When opening an issue, include the Kikoda Construct Library version, Node version, and stack trace if available. In addition, include the steps to reproduce when appropriate.
+
+You can also open an issue for a feature request.
+
+## Contributing
+
+If you find an issue with this package and have a fix, please feel free to open a pull request following the [procedures](CONTRIBUTING.md).
+
+## Testing
+
+If you contribute to this package you can run the tests using `yarn test`.
+
+## License
+
+Unless explicitly stated otherwise all files in this repository are licensed under the Apache License Version 2.0.
+
+This product includes software developed at Kikoda (https://www.kikoda.com). Copyright 2022 Kikoda, LLC.
